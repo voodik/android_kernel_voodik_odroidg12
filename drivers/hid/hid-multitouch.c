d5e347c427a4eac6e03967e8e01cc51f82097533
codewalker <codewalker@hardkernel.com>
2016-09-06 09:45:45 +0900

    ODROID: Add multitouch V/PIDs.
    /* FITOUCH */
    USB_VENDOR_ID_FITOUCH 0x10c4
    USB_DEVICE_ID_FITOUCH_TOUCHSCREEN_SG 0x5e50
    USB_DEVICE_ID_FITOUCH_TOUCHSCREEN_MT 0x5e51
    USB_DEVICE_ID_FITOUCH_TOUCHSCREEN_Z_MT 0x5e52
    
    Change-Id: I95e27311ad73cef59a0da128ae1c3deda5f8fdd3

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1592,6 +1592,46 @@ static const struct hid_device_id mt_devices[] = {
 		HID_USB_DEVICE(USB_VENDOR_ID_DELL_P2314T,
 				USB_DEVICE_ID_P2314T_TOUCH) },
 
+	 /* DMeasyTouch USB capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_DMEASY,
+				USB_DEVICE_ID_DMEASY_TOUCH) },
+
+	 /* ELO USB capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_ELO_1002L,
+				USB_DEVICE_ID_ELO_1002L_TOUCH) },
+
+	 /* LG 23ET63V USB capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_LG_23ET63V,
+				USB_DEVICE_ID_LG_23ET63V_TOUCH) },
+
+/* Lilliput 779GL-70NP/C/T - 7" HDMI capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_LILLIPUT,
+				USB_DEVICE_ID_LILLIPUT_779GL_TOUCH) },
+
+/* Zaagtech HDMI capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_ZAAGTECH,
+				USB_DEVICE_ID_ZAAGTECH_TOUCH) },
+
+/* FITOUCH HDMI capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_FITOUCH,
+				USB_DEVICE_ID_FITOUCH_TOUCHSCREEN_SG) },
+
+/* FITOUCH HDMI capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_FITOUCH,
+				USB_DEVICE_ID_FITOUCH_TOUCHSCREEN_MT) },
+
+/* FITOUCH HDMI capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_FITOUCH,
+				USB_DEVICE_ID_FITOUCH_TOUCHSCREEN_Z_MT) },
+
 	/* Generic MT device */
 	{ HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH, HID_ANY_ID, HID_ANY_ID) },
 
