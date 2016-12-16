9ac38da4f84b3cf868fdf64db5411c70c73221fe
Miklos Szeredi <mszeredi@redhat.com>
2016-12-16 11:02:54 +0100

    vfs: no mnt_want_write_file() in vfs_{copy,clone}_file_range()
    We've checked for file_out being opened for write.  This ensures that we
    already have mnt_want_write() on target.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -1544,9 +1544,7 @@ ssize_t vfs_copy_file_range(struct file *file_in, loff_t pos_in,
 	if (len == 0)
 		return 0;
 
-	ret = mnt_want_write_file(file_out);
-	if (ret)
-		return ret;
+	sb_start_write(inode_out->i_sb);
 
 	/*
 	 * Try cloning first, this is supported by more file systems, and
@@ -1582,7 +1580,7 @@ done:
 	inc_syscr(current);
 	inc_syscw(current);
 
-	mnt_drop_write_file(file_out);
+	sb_end_write(inode_out->i_sb);
 
 	return ret;
 }
@@ -1816,9 +1814,7 @@ int vfs_clone_file_range(struct file *file_in, loff_t pos_in,
 	if (pos_in + len > i_size_read(inode_in))
 		return -EINVAL;
 
-	ret = mnt_want_write_file(file_out);
-	if (ret)
-		return ret;
+	sb_start_write(inode_out->i_sb);
 
 	ret = file_in->f_op->clone_file_range(file_in, pos_in,
 			file_out, pos_out, len);
@@ -1827,7 +1823,7 @@ int vfs_clone_file_range(struct file *file_in, loff_t pos_in,
 		fsnotify_modify(file_out);
 	}
 
-	mnt_drop_write_file(file_out);
+	sb_end_write(inode_out->i_sb);
 	return ret;
 }
 EXPORT_SYMBOL(vfs_clone_file_range);
