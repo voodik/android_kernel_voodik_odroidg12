8d88a3723d408cadade28f53d580d1910c985a0a
Markus Elfring <elfring@users.sourceforge.net>
2017-05-09 17:48:45 -0700

    Input: use seq_putc() in input_seq_print_bitmap()
    Switch to using seq_putc() when printing a single character '0'.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1150,7 +1150,7 @@ static void input_seq_print_bitmap(struct seq_file *seq, const char *name,
 	 * If no output was produced print a single 0.
 	 */
 	if (skip_empty)
-		seq_puts(seq, "0");
+		seq_putc(seq, '0');
 
 	seq_putc(seq, '\n');
 }
