feea4a23d350d8adef223841d9fa0152146e123b
Neil Horman <nhorman@tuxdriver.com>
2017-06-30 13:32:57 -0400

    sctp: Add peeloff-flags socket option
    Based on a request raised on the sctp devel list, there is a need to
    augment the sctp_peeloff operation while specifying the O_CLOEXEC and
    O_NONBLOCK flags (simmilar to the socket syscall).  Since modifying the
    SCTP_SOCKOPT_PEELOFF socket option would break user space ABI for existing
    programs, this patch creates a new socket option
    SCTP_SOCKOPT_PEELOFF_FLAGS, which accepts a third flags parameter to
    allow atomic assignment of the socket descriptor flags.
    
    Tested successfully by myself and the requestor
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    CC: Vlad Yasevich <vyasevich@gmail.com>
    CC: "David S. Miller" <davem@davemloft.net>
    CC: Andreas Steinmetz <ast@domdv.de>
    CC: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Acked-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/uapi/linux/sctp.h
+++ include/uapi/linux/sctp.h
@@ -115,6 +115,7 @@ typedef __s32 sctp_assoc_t;
 #define SCTP_PR_SUPPORTED	113
 #define SCTP_DEFAULT_PRINFO	114
 #define SCTP_PR_ASSOC_STATUS	115
+#define SCTP_SOCKOPT_PEELOFF_FLAGS 122
 
 /* PR-SCTP policies */
 #define SCTP_PR_SCTP_NONE	0x0000
@@ -916,6 +917,11 @@ typedef struct {
 	sctp_assoc_t associd;
 	int sd;
 } sctp_peeloff_arg_t;
+
+typedef struct {
+	sctp_peeloff_arg_t p_arg;
+	unsigned flags;
+} sctp_peeloff_flags_arg_t;
 
 /*
  *  Peer Address Thresholds socket option
