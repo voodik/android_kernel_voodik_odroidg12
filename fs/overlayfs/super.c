079988c00bedb547acafab36e87e295940c98f1a
Miklos Szeredi <mszeredi@redhat.com>
2017-11-09 10:23:28 +0100

    ovl: use path_put_init() in error paths for ovl_fill_super()
    This allows simplifying the error cleanup later.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -584,7 +584,7 @@ static int ovl_mount_dir_noesc(const char *name, struct path *path)
 	return 0;
 
 out_put:
-	path_put(path);
+	path_put_init(path);
 out:
 	return err;
 }
@@ -602,7 +602,7 @@ static int ovl_mount_dir(const char *name, struct path *path)
 			if (ovl_dentry_remote(path->dentry)) {
 				pr_err("overlayfs: filesystem on '%s' not supported as upperdir\n",
 				       tmp);
-				path_put(path);
+				path_put_init(path);
 				err = -EINVAL;
 			}
 		kfree(tmp);
@@ -654,7 +654,7 @@ static int ovl_lower_dir(const char *name, struct path *path,
 	return 0;
 
 out_put:
-	path_put(path);
+	path_put_init(path);
 out:
 	return err;
 }
