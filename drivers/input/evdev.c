546e21137acfd0b79cdfb342f0d6207f03ea6323
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2018-10-04 17:45:54 -0700

    Input: evdev - add a schedule point in evdev_write()
    Large writes to evdev interface may cause rcu stalls. Let's add
    cond_resched() to the loop to avoid this.
    
    Reviewed-by: Paul E. McKenney <paulmck@linux.ibm.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -554,6 +554,7 @@ static ssize_t evdev_write(struct file *file, const char __user *buffer,
 
 		input_inject_event(&evdev->handle,
 				   event.type, event.code, event.value);
+		cond_resched();
 	}
 
  out:
