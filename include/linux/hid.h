2acfb04625cf448a56b95054612800e0770f8ea5
Peter Hutterer <peter.hutterer@who-t.net>
2018-12-05 10:42:22 +1000

    HID: core: store the collections as a basic tree
    For each collection parsed, store a pointer to the parent collection
    (if any). This makes it a lot easier to look up which collection(s)
    any given item is part of
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Verified-by: Harry Cutts <hcutts@chromium.org>
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>

--- include/linux/hid.h
+++ include/linux/hid.h
@@ -425,6 +425,7 @@ struct hid_local {
  */
 
 struct hid_collection {
+	struct hid_collection *parent;
 	unsigned type;
 	unsigned usage;
 	unsigned level;
@@ -645,6 +646,7 @@ struct hid_parser {
 	unsigned int         *collection_stack;
 	unsigned int          collection_stack_ptr;
 	unsigned int          collection_stack_size;
+	struct hid_collection *active_collection;
 	struct hid_device    *device;
 	unsigned int          scan_flags;
 };
