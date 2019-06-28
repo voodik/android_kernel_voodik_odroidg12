4d1e3c608c83e99fc3615cb952c48bb0e8289ffd
Kevin.Kim <ckkim@hardkernel.com>
2019-06-28 14:21:21 +0900

    ODROID-C4: Add sound card driver
    Change-Id: Ie91640c3f324076076be6b19b71eb54a74483e1f

--- sound/soc/amlogic/auge/spdif_hw.c
+++ sound/soc/amlogic/auge/spdif_hw.c
@@ -664,7 +664,7 @@ void spdifout_play_with_zerodata(unsigned int spdif_id, bool reenable)
 		/* spdif clk */
 		//spdifout_clk_ctrl(spdif_id, true);
 		/* spdif to hdmitx */
-#if defined(CONFIG_ARCH_MESON64_ODROID_COMMON)
+#if defined(CONFIG_ARCH_MESON64_ODROIDN2)
 		/* ODROID spdif_b only to hdmitx */
 		if (spdif_id == 1)
 #endif
