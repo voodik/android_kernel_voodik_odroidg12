63faaf246895bb98a014a493afc7ff5a7c973ffe
voodik <voodik.am@gmail.com>
2025-01-17 07:18:12 +0500

    ODROID: ovl: more fixes after merge

--- fs/open.c
+++ fs/open.c
@@ -923,7 +923,7 @@ EXPORT_SYMBOL(file_path);
 int vfs_open(const struct path *path, struct file *file,
 	     const struct cred *cred)
 {
-	struct dentry *dentry = d_real(path->dentry, NULL, file->f_flags);
+	struct dentry *dentry = d_real(path->dentry, NULL);
 
 	if (IS_ERR(dentry))
 		return PTR_ERR(dentry);
